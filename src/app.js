add9174881963e4be6f90fbf558c0d546e7ab1f0
Rodrigo Ribeiro <suporte.rodrigoribeiro@gmail.com>
2020-10-26 22:07:37 -0300

    Desafio finalizado.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +9,77 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(request, response, next){
+  const { id } = request.params;
+  if (!isUuid(id)){
+      return response.status(400).json({ "error" : "Invalid repository ID." });
+  }
+  return next();
+}
+
+app.use('/repositories/:id', validateRepositoryId);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  const { title } = request.query;
+  console.log(title);
+  const results = title 
+      ? repositories.filter(repository => repository.title.includes(title)) 
+      : repositories;
+
+  return response.json(results);  
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs }  = request.body;
+  const repository = { id: uuid(), title , url, techs, likes: 0 };
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs }  = request.body;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  
+  const repository = {
+    id, 
+    title, 
+    url, 
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({"erro" : "Repository does not exists."}); 
+  }
+
+  repositories[repositoryIndex] = repository;
+  return response.json(repository);  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex >= 0) {
+    repositories.splice(repositoryIndex,1);
+  } else { 
+    return response.status(400).json({"erro" : "Repository does not exists."}); 
+  }
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex === -1) {
+    return response.status(400).json({"erro" : "Repository does not exists."}); 
+  }
+
+  repositories[repositoryIndex].likes++;
+  
+  return response.json(repositories[repositoryIndex]);
 });
 
 module.exports = app;
